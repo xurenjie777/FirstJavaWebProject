b72c1794e8ab0983527602e45ca1f21478031c5d
xurenjie777 <1102297163@qq.com>
2021-03-11 23:41:39 +0800

    3.11commit

---
+++ src/main/java/com/example/demo/mapper/StaffMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+import java.util.List;
+import com.example.demo.pojo.Staff;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StaffMapper {
+    public int add(Staff staff);
+    public void delete(int id);
+    public Staff get(int id);
+    public int update(Staff staff);
+    public List<Staff> list();
+
+}
---
+++ src/main/java/com/example/demo/pojo/Staff.java
@@ -0,0 +1,23 @@
+package com.example.demo.pojo;
+
+public class Staff {
+    private int id;
+    private String name;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Hero [id=" + id + ", name=" + name  + "]";
+    }
+}
+
